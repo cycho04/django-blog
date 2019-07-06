32388e356ba222bbf6d9f7d7d90e6916dde8aad6
Chris Cho <chrisycho04@gmail.com>
2019-07-05 23:30:19 -0700

    models and linked to admin site

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'CC',
-        'title': 'Blog Post 1',
-        'content': 'Content here',
-        'date_posted': '7/4/2019'
-    },
-    {
-        'author': 'CC',
-        'title': 'Blog Post 2',
-        'content': 'Content here for blog 2',
-        'date_posted': '7/5/2019'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
         'title': 'Testing'
     }
     return render(request, 'blog/home.html', context)
